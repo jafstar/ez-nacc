a6b1edf05155adcd98d9046969c23f3bf6bba902
jafstar <jafar104@gmail.com>
2024-01-31 09:15:58 -0500

    Add Fixed Logo, add react-helmet, add keys to Page comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "react-scroll-to-top";
-import WOW from 'wowjs';
+import WOW from "wowjs";
 import { FaAngleUp } from "react-icons/fa";
 
 //Pages
 import HomeDefault from "./pages/HomeDefault";
+import Page from "./views/Page";
+
 import HomeTwo from "./pages/HomeTwo";
 import HomeThree from "./pages/HomeThree";
 import About from "./pages/About";
@@ -26,47 +28,131 @@ import BlogSidebar from "./pages/BlogSidebar";
 import BlogDetails from "./pages/BlogDetails";
 import Contact from "./pages/Contact";
 import Error from "./pages/Error";
-import ScrollToTopRoute from './components/scroll-to-top-route/ScrollToTopRoute';
+import ScrollToTopRoute from "./components/scroll-to-top-route/ScrollToTopRoute";
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import 'swiper/css/effect-fade';
+import "swiper/css/effect-fade";
 
 function App() {
   useEffect(() => {
     new WOW.WOW({
-        live: false
-      }).init();
-    }, []
-  );
+      live: false,
+    }).init();
+  }, []);
   return (
     <Router>
       <ScrollToTopRoute />
-        <Routes>
-            <Route path={`${process.env.PUBLIC_URL}/`} exact element={<HomeDefault />} />
-            <Route path={`${process.env.PUBLIC_URL}/home-two`} exact element={<HomeTwo />} />
-            <Route path={`${process.env.PUBLIC_URL}/home-three`} exact element={<HomeThree />} />
-            <Route path={`${process.env.PUBLIC_URL}/about`} exact element={<About />} />
-            <Route path={`${process.env.PUBLIC_URL}/team`} exact element={<Team />} />
-            <Route path={`${process.env.PUBLIC_URL}/team-details`} exact element={<TeamDetails />} />
-            <Route path={`${process.env.PUBLIC_URL}/projects`} exact element={<Projects />} />
-            <Route path={`${process.env.PUBLIC_URL}/project-details`} exact element={<ProjectDetails />} />
-            <Route path={`${process.env.PUBLIC_URL}/testimonials`} exact element={<Testimonials />} />
-            <Route path={`${process.env.PUBLIC_URL}/gallery`} exact element={<Gallery />} />
-            <Route path={`${process.env.PUBLIC_URL}/donation`} exact element={<Donation />} />
-            <Route path={`${process.env.PUBLIC_URL}/donation-list`} exact element={<DonationList />} />
-            <Route path={`${process.env.PUBLIC_URL}/donation-details`} exact element={<DonationDetails />} />
-            <Route path={`${process.env.PUBLIC_URL}/donate-now`} exact element={<DonateNow />} />
-            <Route path={`${process.env.PUBLIC_URL}/Events`} exact element={<Events />} />
-            <Route path={`${process.env.PUBLIC_URL}/blog-v-1`} exact element={<BlogVersionOne />} />
-            <Route path={`${process.env.PUBLIC_URL}/blog-v-2`} exact element={<BlogVersionTwo />} />
-            <Route path={`${process.env.PUBLIC_URL}/blog-sidebar`} exact element={<BlogSidebar />} />
-            <Route path={`${process.env.PUBLIC_URL}/blog-details`} exact element={<BlogDetails />} />
-            <Route path={`${process.env.PUBLIC_URL}/contact`} exact element={<Contact />} />
-            <Route path={`${process.env.PUBLIC_URL}/*`} exact element={<Error />} />
-        </Routes>
-        <ScrollToTop className="scrollUp" smooth top="1500" component={<FaAngleUp />} />
+      <Routes>
+        <Route
+          path={`${process.env.PUBLIC_URL}/`}
+          exact
+          element={<Page key="page-home" />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/home-two`}
+          exact
+          element={<HomeTwo />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/home-three`}
+          exact
+          element={<HomeThree />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/about`}
+          exact
+          element={<Page key="page-about" />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/team`}
+          exact
+          element={<Team />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/team-details`}
+          exact
+          element={<TeamDetails />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/projects`}
+          exact
+          element={<Projects />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/project-details`}
+          exact
+          element={<ProjectDetails />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/testimonials`}
+          exact
+          element={<Testimonials />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/gallery`}
+          exact
+          element={<Gallery />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/donation`}
+          exact
+          element={<Donation />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/donation-list`}
+          exact
+          element={<DonationList />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/donation-details`}
+          exact
+          element={<DonationDetails />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/donate-now`}
+          exact
+          element={<DonateNow />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/Events`}
+          exact
+          element={<Events />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/blog-v-1`}
+          exact
+          element={<BlogVersionOne />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/blog-v-2`}
+          exact
+          element={<BlogVersionTwo />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/blog-sidebar`}
+          exact
+          element={<BlogSidebar />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/blog-details`}
+          exact
+          element={<BlogDetails />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/contact`}
+          exact
+          element={<Contact />}
+        />
+        <Route path={`${process.env.PUBLIC_URL}/*`} exact element={<Error />} />
+      </Routes>
+      <ScrollToTop
+        className="scrollUp"
+        smooth
+        top="1500"
+        component={<FaAngleUp />}
+      />
     </Router>
   );
 }
--- src/views/Page.js
+++ src/views/Page.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { lazy, LazyBoundary } from "react-imported-component";
+import { lazy } from "react-imported-component";
+import { Helmet } from "react-helmet";
 
-const HomeDefault = () => {
+const Page = () => {
   const [pageData, setPageData] = React.useState(null);
 
   React.useEffect(() => {
-    const API_BASE = `https://gnbxwvzlnzromuawcswx.supabase.co/functions/v1`;
-    const URL = `${API_BASE}/pages/index`;
+    console.log(document.location);
 
-    const res = fetch(URL, {
+    const DOCPATH = document.location.pathname;
+    const API_BASE = `https://gnbxwvzlnzromuawcswx.supabase.co/functions/v1`;
+    const PATHNAME = DOCPATH === "/" ? "/index" : DOCPATH;
+    const URL = `${API_BASE}/pages${PATHNAME}`;
+
+    console.log("URL: ", URL);
+
+    fetch(URL, {
       method: "POST",
       mode: "cors",
       // headers: {
@@ -43,6 +50,10 @@ const HomeDefault = () => {
         pageData.components.map((Itm, idx) => {
           return (
             <div key={`itm-${idx}`}>
+              <Helmet>
+                <meta charSet="utf-8" />
+                <title>{pageData.title}</title>
+              </Helmet>
               <Itm content={pageData.content[idx].comp_content} />
             </div>
           );
@@ -51,4 +62,4 @@ const HomeDefault = () => {
   );
 };
 
-export default HomeDefault;
+export default Page;
